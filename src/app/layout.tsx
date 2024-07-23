cc4b46de7da4cc6877b7d4de19acb3f8149aafb5
JupiterIT5 <geno74arhipov@gmail.com>
2024-07-23 21:27:32 +0500

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,10 @@ export default function RootLayout({children}: PropsWithChildren) {
       <head>
         <meta name='title' content='garandbuild'/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
       </head>
       <body className='container'>
