325140969c34e707efd483b71a557c2e75f4cd43
JupiterIT5 <geno74arhipov@gmail.com>
2024-07-23 22:05:18 +0500

    update style

--- src/components/Home/Services/Services.tsx
+++ src/components/Home/Services/Services.tsx
@@ -4,12 +4,9 @@ import cl from './services.module.scss'
 import array from '../../../image/icon/array3.svg'
 import { language } from '@/components/Hooks/useLanguage'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Services() {
-	const navigateToPage = (category: string): undefined => {
-		window.location.href = `http://localhost:3000/catalog/${category}`
-	}
-
 	return (
 		<section className={cl.section__service} id='services'>
 			<h2 className={cl.title}>
@@ -18,179 +15,180 @@ export default function Services() {
 				{language() === 'ES' ? 'Nuestros servicios' : ''}
 			</h2>
 			<div className={cl.service__container}>
-				<div className={cl.service} onClick={() => navigateToPage('plumbing')}>
-					<div className={[cl.service1, cl.service_img].join(' ')}></div>
-					<h2>
-						{language() === 'RU' ? 'Сантехника' : ''}{' '}
-						{language() === 'EN' ? 'Plumbing' : ''}{' '}
-						{language() === 'ES' ? 'Plomería' : ''}
-					</h2>
-					<p>
-						{language() === 'RU'
-							? 'Полный комплекс сантехнических услуг: от монтажа водонагревателей до установки систем отопления'
-							: ''}{' '}
-						{language() === 'EN'
-							? 'A full range of plumbing services: from installation of water heaters to installation of heating systems'
-							: ''}{' '}
-						{language() === 'ES'
-							? 'Una gama completa de servicios de fontanería: desde la instalación de calentadores de agua hasta la instalación de sistemas de calefacción.'
-							: ''}
-					</p>
-					<Image src={array} alt='array' className={cl.array} />
-				</div>
-				<div className={cl.service} onClick={() => navigateToPage('ceilings')}>
-					<div className={[cl.service2, cl.service_img].join(' ')}></div>
-					<h2>
-						{language() === 'RU' ? 'Потолки' : ''}{' '}
-						{language() === 'EN' ? 'Ceilings' : ''}{' '}
-						{language() === 'ES' ? '' : 'Techos'}
-					</h2>
-					<p>
-						{language() === 'RU'
-							? 'Профессиональная отделка потолков: натяжные конструкции, гипсокартон, покраска'
-							: ''}{' '}
-						{language() === 'EN'
-							? 'Professional finishing of ceilings: tension structures, plasterboard, painting'
-							: ''}{' '}
-						{language() === 'ES'
-							? 'Acabado profesional de techos: estructuras tensadas, placas de yeso, pintura.'
-							: ''}
-					</p>
-					<Image src={array} alt='array' className={cl.array} />
-				</div>
-				<div
-					className={cl.service}
-					onClick={() => navigateToPage('buildHouses')}
-				>
-					<div className={[cl.service3, cl.service_img].join(' ')}></div>
-					<h2>
-						{language() === 'RU' ? 'Строительство домов' : ''}{' '}
-						{language() === 'EN' ? 'Construction of houses' : ''}{' '}
-						{language() === 'ES' ? 'Construcción de casas' : ''}
-					</h2>
-					<p>
-						{language() === 'RU' ? 'Строительство домов любой сложности' : ''}{' '}
-						{language() === 'EN'
-							? 'Construction of houses of any complexity'
-							: ''}{' '}
-						{language() === 'ES'
-							? 'Construcción de viviendas de cualquier complejidad.'
-							: ''}
-					</p>
-					<Image src={array} alt='array' className={cl.array} />
-				</div>
-				<div
-					className={cl.service}
-					onClick={() => navigateToPage('electrician')}
-				>
-					<div className={[cl.service4, cl.service_img].join(' ')}></div>
-					<h2>
-						{language() === 'RU' ? 'Электрик' : ''}{' '}
-						{language() === 'EN' ? 'Electrician' : ''}{' '}
-						{language() === 'ES' ? 'Electricista' : ''}
-					</h2>
-					<p>
-						{language() === 'RU'
-							? 'Все виды электромонтажных работ для вашего дома и бизнеса'
-							: ''}{' '}
-						{language() === 'EN'
-							? 'All types of electrical work for your home and business'
-							: ''}{' '}
-						{language() === 'ES'
-							? 'Todo tipo de trabajos eléctricos para tu hogar y negocio.'
-							: ''}
-					</p>
-					<Image src={array} alt='array' className={cl.array} />
-				</div>
-				<div
-					className={cl.service}
-					onClick={() => navigateToPage('renovationWork')}
-				>
-					<div className={[cl.service5, cl.service_img].join(' ')}></div>
-					<h2>
-						{language() === 'RU' ? 'Ремонтные работы' : ''}{' '}
-						{language() === 'EN' ? 'Repair work' : ''}{' '}
-						{language() === 'ES' ? 'Trabajo de reparación' : ''}
-					</h2>
-					<p>
-						{language() === 'RU'
-							? 'Профессионально выполняем весь спектр . ремонтных работ для дома и офисов'
-							: ''}{' '}
-						{language() === 'EN'
-							? 'We carry out the entire range professionally. renovation work for home and offices'
-							: ''}{' '}
-						{language() === 'ES'
-							? 'Realizamos toda la gama de forma profesional. trabajos de reformas para el hogar y oficinas'
-							: ''}
-					</p>
-					<Image src={array} alt='array' className={cl.array} />
-				</div>
-				<div className={cl.service} onClick={() => navigateToPage('facades')}>
-					<div className={[cl.service6, cl.service_img].join(' ')}></div>
-					<h2>
-						{language() === 'RU' ? 'Фасады' : ''}{' '}
-						{language() === 'EN' ? 'Facades' : ''}{' '}
-						{language() === 'ES' ? 'Fachadas' : ''}
-					</h2>
-					<p>
-						{language() === 'RU'
-							? 'Фасадные работы: очистка, декоративная отделка, покраска, гидроизоляция.'
-							: ''}{' '}
-						{language() === 'EN'
-							? 'Facade work: cleaning, decorative finishing, painting, waterproofing.'
-							: ''}{' '}
-						{language() === 'ES'
-							? 'Trabajos de fachada: limpieza, acabados decorativos, pintura, impermeabilización.'
-							: ''}
-					</p>
-					<Image src={array} alt='array' className={cl.array} />
-				</div>
-				<div
-					className={cl.service}
-					onClick={() => navigateToPage('labdscapingWorks')}
-				>
-					<div className={[cl.service7, cl.service_img].join(' ')}></div>
-					<h2>
-						{language() === 'RU' ? 'Ландшафтные работы' : ''}{' '}
-						{language() === 'EN' ? 'Landscaping works' : ''}{' '}
-						{language() === 'ES' ? 'Obras de paisajismo' : ''}
-					</h2>
-					<p>
-						{language() === 'RU'
-							? 'Преобразим ваш участок: озеленение, автоматический полив, освещение и тротуарные дорожки.'
-							: ''}{' '}
-						{language() === 'EN'
-							? 'We will transform your site: landscaping, automatic watering, lighting and sidewalk paths.'
-							: ''}{' '}
-						{language() === 'ES'
-							? 'Transformaremos su sitio: paisajismo, riego automático, iluminación y senderos para aceras.'
-							: ''}
-					</p>
-					<Image src={array} alt='array' className={cl.array} />
-				</div>
-				<div
-					className={cl.service}
-					onClick={() => navigateToPage('buildPools')}
-				>
-					<div className={[cl.service8, cl.service_img].join(' ')}></div>
-					<h2>
-						{language() === 'RU' ? 'Строительство бассейнов' : ''}{' '}
-						{language() === 'EN' ? 'Construction of swimming pools' : ''}{' '}
-						{language() === 'ES' ? 'Construcción de piscinas' : ''}
-					</h2>
-					<p>
-						{language() === 'RU'
-							? 'Профессиональное строительство бассейнов любого объема'
-							: ''}{' '}
-						{language() === 'EN'
-							? 'Professional construction of swimming pools of any size'
-							: ''}{' '}
-						{language() === 'ES'
-							? 'Construcción profesional de piscinas de cualquier tamaño.'
-							: ''}
-					</p>
-					<Image src={array} alt='array' className={cl.array} />
-				</div>
+				<Link href={'catalog/plumbing'}>
+					<div className={cl.service}>
+						<div className={[cl.service1, cl.service_img].join(' ')}></div>
+						<h2>
+							{language() === 'RU' ? 'Сантехника' : ''}{' '}
+							{language() === 'EN' ? 'Plumbing' : ''}{' '}
+							{language() === 'ES' ? 'Plomería' : ''}
+						</h2>
+						<p>
+							{language() === 'RU'
+								? 'Полный комплекс сантехнических услуг: от монтажа водонагревателей до установки систем отопления'
+								: ''}{' '}
+							{language() === 'EN'
+								? 'A full range of plumbing services: from installation of water heaters to installation of heating systems'
+								: ''}{' '}
+							{language() === 'ES'
+								? 'Una gama completa de servicios de fontanería: desde la instalación de calentadores de agua hasta la instalación de sistemas de calefacción.'
+								: ''}
+						</p>
+						<Image src={array} alt='array' className={cl.array} />
+					</div>
+				</Link>
+				<Link href={'catalog/ceilings'}>
+					<div className={cl.service}>
+						<div className={[cl.service2, cl.service_img].join(' ')}></div>
+						<h2>
+							{language() === 'RU' ? 'Потолки' : ''}{' '}
+							{language() === 'EN' ? 'Ceilings' : ''}{' '}
+							{language() === 'ES' ? 'Techos' : ''}
+						</h2>
+						<p>
+							{language() === 'RU'
+								? 'Профессиональная отделка потолков: натяжные конструкции, гипсокартон, покраска'
+								: ''}{' '}
+							{language() === 'EN'
+								? 'Professional finishing of ceilings: tension structures, plasterboard, painting'
+								: ''}{' '}
+							{language() === 'ES'
+								? 'Acabado profesional de techos: estructuras tensadas, placas de yeso, pintura.'
+								: ''}
+						</p>
+						<Image src={array} alt='array' className={cl.array} />
+					</div>
+				</Link>
+				<Link href={'catalog/buildHouses'}>
+					<div className={cl.service}>
+						<div className={[cl.service3, cl.service_img].join(' ')}></div>
+						<h2>
+							{language() === 'RU' ? 'Строительство домов' : ''}{' '}
+							{language() === 'EN' ? 'Construction of houses' : ''}{' '}
+							{language() === 'ES' ? 'Construcción de casas' : ''}
+						</h2>
+						<p>
+							{language() === 'RU' ? 'Строительство домов любой сложности' : ''}{' '}
+							{language() === 'EN'
+								? 'Construction of houses of any complexity'
+								: ''}{' '}
+							{language() === 'ES'
+								? 'Construcción de viviendas de cualquier complejidad.'
+								: ''}
+						</p>
+						<Image src={array} alt='array' className={cl.array} />
+					</div>
+				</Link>
+				<Link href={'catalog/electrician'}>
+					<div className={cl.service}>
+						<div className={[cl.service4, cl.service_img].join(' ')}></div>
+						<h2>
+							{language() === 'RU' ? 'Электрик' : ''}{' '}
+							{language() === 'EN' ? 'Electrician' : ''}{' '}
+							{language() === 'ES' ? 'Electricista' : ''}
+						</h2>
+						<p>
+							{language() === 'RU'
+								? 'Все виды электромонтажных работ для вашего дома и бизнеса'
+								: ''}{' '}
+							{language() === 'EN'
+								? 'All types of electrical work for your home and business'
+								: ''}{' '}
+							{language() === 'ES'
+								? 'Todo tipo de trabajos eléctricos para tu hogar y negocio.'
+								: ''}
+						</p>
+						<Image src={array} alt='array' className={cl.array} />
+					</div>
+				</Link>
+				<Link href={'catalog/renovationWork'}>
+					<div className={cl.service}>
+						<div className={[cl.service5, cl.service_img].join(' ')}></div>
+						<h2>
+							{language() === 'RU' ? 'Ремонтные работы' : ''}{' '}
+							{language() === 'EN' ? 'Repair work' : ''}{' '}
+							{language() === 'ES' ? 'Trabajo de reparación' : ''}
+						</h2>
+						<p>
+							{language() === 'RU'
+								? 'Профессионально выполняем весь спектр . ремонтных работ для дома и офисов'
+								: ''}{' '}
+							{language() === 'EN'
+								? 'We carry out the entire range professionally. renovation work for home and offices'
+								: ''}{' '}
+							{language() === 'ES'
+								? 'Realizamos toda la gama de forma profesional. trabajos de reformas para el hogar y oficinas'
+								: ''}
+						</p>
+						<Image src={array} alt='array' className={cl.array} />
+					</div>
+				</Link>
+				<Link href={'catalog/facades'}>
+					<div className={cl.service}>
+						<div className={[cl.service6, cl.service_img].join(' ')}></div>
+						<h2>
+							{language() === 'RU' ? 'Фасады' : ''}{' '}
+							{language() === 'EN' ? 'Facades' : ''}{' '}
+							{language() === 'ES' ? 'Fachadas' : ''}
+						</h2>
+						<p>
+							{language() === 'RU'
+								? 'Фасадные работы: очистка, декоративная отделка, покраска, гидроизоляция.'
+								: ''}{' '}
+							{language() === 'EN'
+								? 'Facade work: cleaning, decorative finishing, painting, waterproofing.'
+								: ''}{' '}
+							{language() === 'ES'
+								? 'Trabajos de fachada: limpieza, acabados decorativos, pintura, impermeabilización.'
+								: ''}
+						</p>
+						<Image src={array} alt='array' className={cl.array} />
+					</div>
+				</Link>
+				<Link href={'catalog/labdscapingWorks'}>
+					<div className={cl.service}>
+						<div className={[cl.service7, cl.service_img].join(' ')}></div>
+						<h2>
+							{language() === 'RU' ? 'Ландшафтные работы' : ''}{' '}
+							{language() === 'EN' ? 'Landscaping works' : ''}{' '}
+							{language() === 'ES' ? 'Obras de paisajismo' : ''}
+						</h2>
+						<p>
+							{language() === 'RU'
+								? 'Преобразим ваш участок: озеленение, автоматический полив, освещение и тротуарные дорожки.'
+								: ''}{' '}
+							{language() === 'EN'
+								? 'We will transform your site: landscaping, automatic watering, lighting and sidewalk paths.'
+								: ''}{' '}
+							{language() === 'ES'
+								? 'Transformaremos su sitio: paisajismo, riego automático, iluminación y senderos para aceras.'
+								: ''}
+						</p>
+						<Image src={array} alt='array' className={cl.array} />
+					</div>
+				</Link>
+				<Link href={'catalog/buildPools'}>
+					<div className={cl.service}>
+						<div className={[cl.service8, cl.service_img].join(' ')}></div>
+						<h2>
+							{language() === 'RU' ? 'Строительство бассейнов' : ''}{' '}
+							{language() === 'EN' ? 'Construction of swimming pools' : ''}{' '}
+							{language() === 'ES' ? 'Construcción de piscinas' : ''}
+						</h2>
+						<p>
+							{language() === 'RU'
+								? 'Профессиональное строительство бассейнов любого объема'
+								: ''}{' '}
+							{language() === 'EN'
+								? 'Professional construction of swimming pools of any size'
+								: ''}{' '}
+							{language() === 'ES'
+								? 'Construcción profesional de piscinas de cualquier tamaño.'
+								: ''}
+						</p>
+						<Image src={array} alt='array' className={cl.array} />
+					</div>
+				</Link>
 			</div>
 		</section>
 	)
